2eab043adaa3d3d11235b3610ac8fcb156d7239d
HY-D1 <93563405+HY-D1@users.noreply.github.com>
2025-10-16 10:09:07 -0700

    update port for later works

--- app.py
+++ app.py
@@ -103,4 +103,7 @@ def static_files(fname):
     return send_from_directory(app.static_folder, fname)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
+
